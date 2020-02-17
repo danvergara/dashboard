79928f2c74ae5c252a14526de2e4366e457bad75
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2020-02-16 22:14:05 -0600

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,34 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+// import Home from '../views/Home.vue';
+import Books from '../components/Books.vue';
+import Ping from '../components/Ping.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
+//  {
+//    path: '/',
+//    name: 'Home',
+//    component: Home,
+//  },
+//  {
+//    path: '/about',
+//    name: 'About',
+//    // route level code-splitting
+//    // this generates a separate chunk (about.[hash].js) for this route
+//    // which is lazy-loaded when the route is visited.
+//    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+//  },
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: 'Books',
+    component: Books,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/ping',
+    name: 'Ping',
+    component: Ping,
   },
 ];
 
