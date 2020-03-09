e17b7049a8fcbcf52acbf432c1957a84ea9b3346
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2020-03-08 18:02:01 -0600

    feat(weather): added the weather component

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" id="navbar-personal">
-      <b-navbar-brand href="#">Personal Dashboard</b-navbar-brand>
+      <b-navbar-brand href="#">Wachtower project</b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
+        <!-- <b-navbar-nav>
           <b-nav-item href="#">Economics</b-nav-item>
           <b-nav-item href="#">News</b-nav-item>
           <b-nav-item href="#">Weather</b-nav-item>
-        </b-navbar-nav>
+        </b-navbar-nav> -->
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
@@ -40,6 +40,6 @@ export default {
 </script>
 <style media="screen">
   #navbar-personal {
-    background-color: #563d7c;
+    background-color: #454e9e;
   }
 </style>
