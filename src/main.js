5ecd352a57c79b3cd9e8fb866419ca8dc3a28be8
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2020-05-27 00:46:30 -0500

    feat(auth0): implement the auth0 protection

--- src/main.js
+++ src/main.js
@@ -5,11 +5,29 @@ import 'vue2-perfect-scrollbar/dist/vue2-perfect-scrollbar.css';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import { Auth0Plugin } from './auth';
 
 Vue.use(BootstrapVue);
 Vue.use(PerfectScrollbar);
 
 Vue.config.productionTip = false;
+
+
+const domain = process.env.VUE_APP_DOMAIN;
+const clientId = process.env.VUE_APP_CLIENT_ID;
+
+
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: (appState) => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname,
+    );
+  },
+});
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import AppNavigation from '../components/AppNavigation.vue';
+import MainContent from '../components/MainContent.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
-//  {
-//    path: '/',
-//    name: 'Home',
-//    component: Home,
-//  },
-//  {
-//    path: '/about',
-//    name: 'About',
-//    // route level code-splitting
-//    // this generates a separate chunk (about.[hash].js) for this route
-//    // which is lazy-loaded when the route is visited.
-//    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-//  },
   {
     path: '/',
-    name: 'AppNavigation',
-    component: AppNavigation,
+    name: 'MainContent',
+    component: MainContent,
   },
 ];
 
