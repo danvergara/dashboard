2175609d3a62027e3d00590fa8bcc7fde65edc14
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2020-05-27 01:30:03 -0500

    fix(env-vars): add the required en vars to vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,8 @@ module.exports = {
       new webpack.DefinePlugin({
         'process.env': {
           VUE_APP_DASHBOARD_SERVER_URL: JSON.stringify(process.env.VUE_APP_DASHBOARD_SERVER_URL),
+          VUE_APP_CLIENT_ID: JSON.stringify(process.env.VUE_APP_CLIENT_ID),
+          VUE_APP_DOMAIN: JSON.stringify(process.env.VUE_APP_DOMAIN),
         },
       }),
     ],
