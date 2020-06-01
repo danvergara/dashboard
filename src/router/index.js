601e912fbf025e848d21c4da6a677c39da0c906c
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2020-06-01 00:37:57 -0500

    feat(login-view): protect the login view if the user is authenticated

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,22 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import MainContent from '../components/MainContent.vue';
+import Login from '../components/Login.vue';
+import { authGuard } from '../auth/authGuard';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
+    name: 'Login',
+    component: Login,
+  },
+  {
+    path: '/dashboard',
     name: 'MainContent',
     component: MainContent,
+    beforeEnter: authGuard,
   },
 ];
 
