a2fa25bc93211613bb8a225cc73fce3c4cc371f0
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2020-07-30 19:50:19 -0500

    feat(components): add repositories component to project

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -2,13 +2,12 @@
   <div class="" id="main-content">
     <navbar></navbar>
     <div class="row justify-content-md-center">
-      <div class="col-lg-5 col-md-12 col-sm-12">
+      <div class="col-lg-7 col-md-12 col-sm-12">
         <news></news>
       </div>
 
-      <div class="col-lg-7 col-md-12 col-sm-12">
-        <economics></economics>
-        <weather></weather>
+      <div class="col-lg-5 col-md-12 col-sm-12">
+        <repositories></repositories>
       </div>
     </div>
   </div>
@@ -16,9 +15,8 @@
 
 <script>
 import NavBar from './NavBar.vue';
-import Economics from './Economics.vue';
 import News from './News.vue';
-import Weather from './Weather.vue';
+import Repositories from './Repositories.vue';
 
 export default {
   data() {
@@ -26,9 +24,8 @@ export default {
   },
   components: {
     navbar: NavBar,
-    economics: Economics,
     news: News,
-    weather: Weather,
+    repositories: Repositories,
   },
 };
 </script>
